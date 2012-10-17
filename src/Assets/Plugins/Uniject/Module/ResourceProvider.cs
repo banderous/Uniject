6f46f3146870c1da39a2aafa062f537e3ecdd08f
Alex McAusland <alex.mcausland@gmail.com>
2012-10-17 11:38:09 +0100

    Added Resource attribute for direct injection of automatically loaded resources such as AudioClips. Updated example scene to play an audio clip every time the sphere resets position.

---
+++ src/Assets/Plugins/Uniject/Module/ResourceProvider.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace AssemblyCSharpfirstpassvs {
+    public class ResourceProvider {
+        public ResourceProvider() {
+        }
+    }
+}
+
---
+++ src/test/tests/src/Impl/framework/FileVerifyingResourceProvider.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace test {
+    public class FileVerifyingResourceProvider {
+        public FileVerifyingResourceProvider() {
+        }
+    }
+}
+
