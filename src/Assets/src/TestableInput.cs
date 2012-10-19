5fa84c225fbe2bea6c7d49d618ca33280f81a646
Alex McAusland <alex.mcausland@gmail.com>
2012-10-19 14:13:19 +0100

    Removing use of delta time from applied force example.

--- src/Assets/src/TestableInput.cs
+++ src/Assets/src/TestableInput.cs
@@ -11,18 +11,16 @@ using UnityEngine;
 public class TestableInput : Testable.TestableComponent {
     public Sphere sphere { get; private set; }
     private IInput input;
-    private ITime time;
-    public TestableInput(TestableGameObject obj, IInput input, ITime time,
-                         Sphere s) : base(obj) {
-        this.sphere = s;
+    public TestableInput(TestableGameObject obj, IInput input,
+                         Sphere sphere) : base(obj) {
+        this.sphere = sphere;
         this.input = input;
-        this.time = time;
     }
 
     public override void Update() {
         float deltaX = input.GetAxis("Horizontal");
         float deltaY = input.GetAxis("Vertical");
-        Vector3 forceVec = (Vector3.right * deltaX + Vector3.forward * deltaY) * time.DeltaTime * 200.0f;
+        Vector3 forceVec = (Vector3.right * deltaX + Vector3.forward * deltaY);
         if (forceVec.sqrMagnitude > 0) {
             sphere.body.AddForce(forceVec);
         }
