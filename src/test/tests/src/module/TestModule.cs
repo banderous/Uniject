bf9488c57f9c15226636a4f210ed00e936fd9500
Alan Kendall <alankendall@gmail.com>
2013-01-13 18:00:28 -0600

    Cosmetic whitespace removal

--- src/test/tests/src/module/TestModule.cs
+++ src/test/tests/src/module/TestModule.cs
@@ -15,15 +15,15 @@ namespace Tests {
 
         public override void Load () {
             Rebind<ILayerMask>().To<MockLayerMask>();
-            Rebind<ITime>().To<MockTime> ().InSingletonScope();
+            Rebind<ITime>().To<MockTime>().InSingletonScope();
             Rebind<ILogger>().To<TestLogger>();
             Rebind<IAudioListener>().To<FakeAudioListener>();
-            Rebind<INavmeshAgent>().To<FakeNavmeshAgent> ().InScope(Scoping.GameObjectBoundaryScoper);
+            Rebind<INavmeshAgent>().To<FakeNavmeshAgent>().InScope(Scoping.GameObjectBoundaryScoper);
 
-            Rebind<IRigidBody>().ToProvider<MockProvider<IRigidBody>> ().InScope(Scoping.GameObjectBoundaryScoper);
-            Rebind<ISphereCollider>().ToProvider<MockProvider<ISphereCollider>> ().InScope (Scoping.GameObjectBoundaryScoper);
-            Rebind<IBoxCollider>().ToProvider<MockProvider<IBoxCollider>> ().InScope (Scoping.GameObjectBoundaryScoper);
-            Rebind<ILight>().ToProvider<MockProvider<ILight>> ().InScope (Scoping.GameObjectBoundaryScoper);
+            Rebind<IRigidBody>().ToProvider<MockProvider<IRigidBody>>().InScope(Scoping.GameObjectBoundaryScoper);
+            Rebind<ISphereCollider>().ToProvider<MockProvider<ISphereCollider>>().InScope(Scoping.GameObjectBoundaryScoper);
+            Rebind<IBoxCollider>().ToProvider<MockProvider<IBoxCollider>>().InScope(Scoping.GameObjectBoundaryScoper);
+            Rebind<ILight>().ToProvider<MockProvider<ILight>>().InScope(Scoping.GameObjectBoundaryScoper);
 
             Rebind<IAudioSource>().To<FakeAudioSource>().InScope(Scoping.GameObjectBoundaryScoper);
             Rebind<IUtil>().To<MockUtil>().InSingletonScope();
